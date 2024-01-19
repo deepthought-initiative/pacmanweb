1931e65a20c1a39d0ec98be555e53a0b69be9487
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-19 13:22:58 +0530

    Add a new component for displaying other config options

---
+++ frontend/src/components/otherConfigOptions.jsx
@@ -0,0 +1,14 @@
+import "../css/otherConfigOptions.css";
+
+const OtherConfigOptions = () => {
+  return (
+    <>
+      <div className="separator">Other Options(optional)</div>
+      <div className="row mt-5">
+        <button className="btn">Proposals- Categorize</button>
+      </div>
+    </>
+  )
+}
+
+export default OtherConfigOptions
