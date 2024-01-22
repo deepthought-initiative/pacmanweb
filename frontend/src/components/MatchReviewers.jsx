27cd1691f0d15b099e31106fac3bf8fde5d7224b
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-22 18:03:08 +0530

    Add Match reviewers page.

--- frontend/src/components/MatchReviewers.jsx
+++ frontend/src/components/MatchReviewers.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "../css/searchBox.css";
+import DropdownConfigOption from "./DropdownConfigOption";
+import OtherConfigOptions from "./OtherConfigOptions";
 
 const MatchReviewers = () => {
+  const numbers = [123456, 987654, 456789, 567890, 234567, 890123, 345678, 678901, 789012, 172345];
+
   return (
-    <div>MatchReviewers</div>
+    <div className="mt-5" id="main-container">
+      <form>
+        <div className='row'>
+          <div>
+            <DropdownConfigOption data={numbers} label="Selected Current Cycle" desc="Prefix used throughout script to match with cycle description"/>
+          </div>
+        </div>
+        <OtherConfigOptions />  
+      </form>
+    </div>
   )
 }
 
