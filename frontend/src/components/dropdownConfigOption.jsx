a48feafe723525face702610157fa25a3fc60184
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-22 11:52:38 +0530

    Added css.

--- frontend/src/components/dropdownConfigOption.jsx
+++ frontend/src/components/dropdownConfigOption.jsx
@@ -1,21 +1,21 @@
+import React from 'react';
 import "../css/dropdownConfigOption.css";
 
 const DropdownConfigOption = ({ data }) => {
   return (
-    <div>        
-        <label className="form-label" htmlFor="CurrentCycle">Selected Current Cycle</label>
-        <div className="select-wrapper">
-            <select id="CurrentCycle" className="form-select" aria-label="Select Current Cycle">
-              {data.map((number) => (
-                <option key={number} value={number}>
-                  {number}
-                </option>
-              ))}
-            </select>
-        </div>
+    <div className="dropdown-container">
+      <label className="form-label" htmlFor="CurrentCycle">Selected Current Cycle</label>
+      <div className="select-wrapper">
+        <select id="CurrentCycle" className="form-select" aria-label="Select Current Cycle">
+          {data.map((number) => (
+            <option key={number} value={number}>
+              {number}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default DropdownConfigOption
+export default DropdownConfigOption;
