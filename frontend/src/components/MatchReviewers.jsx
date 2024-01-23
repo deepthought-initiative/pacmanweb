25d3219365355ae851d29c758f6ce3406357d01e
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-23 15:54:39 +0530

    Setup functionality for showing table on button click

--- frontend/src/components/MatchReviewers.jsx
+++ frontend/src/components/MatchReviewers.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "../css/searchBox.css";
 import DropdownConfigOption from "./DropdownConfigOption";
+import OtherConfigOptions from "./OtherConfigOptions";
 import TableMatchReviewers from "./TableMatchReviewers";
 
 const MatchReviewers = () => {
+  const [showTable, setShowTable] = useState(false)
+
   const numbers = [123456, 987654, 456789, 567890, 234567, 890123, 345678, 678901, 789012, 172345];
+  const handleClick = () =>{
+    setShowTable(!showTable)
+  }
 
   return (
     <div className="mt-5" id="main-container">
@@ -13,8 +20,11 @@ const MatchReviewers = () => {
             <DropdownConfigOption data={numbers} label="Selected Current Cycle" desc="Prefix used throughout script to match with cycle description"/>
           </div>
         </div>
-        {/* <OtherConfigOptions button_label="Calculate Reviewers"/>   */}
+        {showTable ?
         <TableMatchReviewers />
+        :
+        <OtherConfigOptions button_label="Categorize Proposal" handleClick={handleClick}/>
+        }
       </form>
     </div>
   )
--- frontend/src/components/ProposalDuplicationChecker.jsx
+++ frontend/src/components/ProposalDuplicationChecker.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "../css/searchBox.css";
 import DropdownConfigOption from "./DropdownConfigOption";
+import OtherConfigOptions from "./OtherConfigOptions";
 import TableForDuplicationChecker from './TableForDuplicationChecker';
 
 const ProposalDuplicationChecker = () => {
+  const [showTable, setShowTable] = useState(false)
+
   const numbers = [123456, 987654, 456789, 567890, 234567, 890123, 345678, 678901, 789012, 172345];
+  const handleClick = () =>{
+    setShowTable(!showTable)
+  }
   
   return (
     <div className="mt-5" id="main-container">
@@ -16,8 +23,11 @@ const ProposalDuplicationChecker = () => {
             <DropdownConfigOption data={numbers} label="Selected Past Cycle" desc="Cycle prefixes of past cycles"/>
           </div>
         </div>
-        {/* <OtherConfigOptions button_label="Find Duplicates"/> */}
+        {showTable ?
         <TableForDuplicationChecker/>
+        :
+        <OtherConfigOptions button_label="Find Duplicates" handleClick={handleClick}/>
+        }
       </form>
     </div>
   )
