aa731c6f6a4a4a84fb5c8c076cdf6f618f01fe60
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-23 14:43:12 +0530

    Updated proposals categorize page

--- frontend/src/components/proposalCategorize.jsx
+++ frontend/src/components/proposalCategorize.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import SearchBox from './searchBox';
+import "../css/searchBox.css";
+import DropdownConfigOption from "./DropdownConfigOption";
+import OtherConfigOptions from "./OtherConfigOptions";
 
 const ProposalCategorize = () => {
+   const numbers = [123456, 987654, 456789, 567890, 234567, 890123, 345678, 678901, 789012, 172345];
   return (
-    <div>
-        <SearchBox/>
+    <div className="mt-5" id="main-container">
+      <form>
+        <div className='row'>
+          <div>
+            <DropdownConfigOption data={numbers} label="Selected Current Cycle" desc="Prefix used throughout script to match with cycle description"/>
+          </div>
+        </div>
+        <OtherConfigOptions button_label="Calculate Reviewers"/>  
+        {/* <TableMatchReviewers /> */}
+      </form>
     </div>
   )
 }
