f832fcc18a2eee999ea680dc88310b36d56ed812
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-30 11:22:11 +0530

    Updated proposal categorize component and display data in proposal table

--- frontend/src/components/ProposalCategorization/ProposalCategorize.jsx
+++ frontend/src/components/ProposalCategorization/ProposalCategorize.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../../css/searchBox.css";
 import DropdownConfigOption from "../util/DropdownConfigOption";
 import Logs from "../util/Logs";
@@ -11,6 +10,23 @@ const ProposalCategorize = () => {
   const [showTable, setShowTable] = useState(false)
   const [showLogs, setShowLogs] = useState(false)
   const [logs, setLogs] = useState([]);
+  const [currentId, setCurrentId] = useState();
+
+  useEffect(() => {
+    async function fetchLogs() {
+      if (!currentId) {
+        return;
+      }
+      const streamResponse = await fetch(`http://127.0.0.1:5000/api/stream/${currentId}?api_key=barebones`, {
+        method: "GET",
+        headers: { Authorization: "Basic " + btoa("default:barebones") },
+      });
+      setShowLogs(true);
+      const allLogs = await streamResponse.text();
+      setLogs(allLogs.split('\n'));
+    }
+    fetchLogs();
+  }, [currentId, setLogs, setShowLogs]);
   
   const numbers = [123456, 987654, 456789, 567890, 234567, 890123, 345678, 678901, 789012, 172345];
   const handleClick = async(event) => {
@@ -21,41 +37,36 @@ const ProposalCategorize = () => {
     headers.append("Content-Type", "application/json");
       
     // Call the first API to get the task ID
-    const spawnResponse = await fetch("http://127.0.0.1:5000/api/run_pacman?categorize_one_cycle=true&get_science_categories=true&main_test_cycle=221026", {
-    method: "GET",
-    headers: { Authorization: "Basic " + btoa("default:barebones") },
+    const spawnResponse = await fetch("http://127.0.0.1:5000/api/run_pacman?mode=PROP&main_test_cycle=221026", {
+      method: "GET",
+      headers: { 
+        Authorization: "Basic " + btoa("default:barebones"),
+        "Content-Type": "application/json"
+       },
     });
 
-  const data = await spawnResponse.json();
-  const currentId = data["result_id"]
-  const streamResponse = await fetch(`http://127.0.0.1:5000/api/stream/${currentId}?api_key=barebones`, {
-    method: "GET",
-    headers: { Authorization: "Basic " + btoa("default:barebones") },
-    });
-    setShowLogs(true)
-    const allLogs = await streamResponse.text();
-    setLogs(allLogs.split('\n'))
+    const data = await spawnResponse.json();
+    setCurrentId(data["result_id"]);
   }
+
   return (
     <div className="mt-5" id="main-container">
-      <form>
         <div className='row'>
           <div>
             <DropdownConfigOption data={numbers} label="Selected Current Cycle" desc="Prefix used throughout script to match with cycle description"/>
           </div>
         </div>
         {showTable ? (
-          <ProposalTable />
+          <ProposalTable currentId={currentId} setShowTable={setShowTable} setShowLogs={setShowLogs}/>
         ) 
         : 
         (
           showLogs ? (
-            <Logs data={logs} setShowTable={setShowTable}/>
+            <Logs data={logs} setShowTable={setShowTable} currentId={currentId}/>
           ) : (
             <OtherConfigOptions button_label="Categorize Proposal" handleClick={handleClick}/>
           )
         )}
-      </form>
     </div>
   )
 }
--- frontend/src/components/ProposalCategorization/ProposalTable.jsx
+++ frontend/src/components/ProposalCategorization/ProposalTable.jsx
@@ -1,96 +1,128 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 import '../../css/proposalTable.css';
+import AlternateCategoriesTest from '../util/AlternateCategoriesText';
 
-// eslint-disable-next-line react/prop-types
-const ProposalTable = ({handleClick}) => {
-    const [highlighted, setHighlighted] = useState()
-    
+const ProposalTable = ({ handleClick, currentId, setShowTable, setShowLogs }) => {
+    const [highlighted, setHighlighted] = useState();
+    const [dataToDisplay, setDataToDisplay] = useState([]);
+
+    useEffect(() => {
+        async function fetchTable() {
+            if (!currentId) {
+                return;
+            }
+            const tableResponse = await fetch(`http://127.0.0.1:5000/api/outputs/proposal_cat_output/${currentId}`, {
+                method: "GET",
+                headers: { Authorization: "Basic " + btoa("default:barebones") },
+            });
+            const tableData = await tableResponse.json();
+            setDataToDisplay(tableData);
+            console.log(tableData)
+        }
+        fetchTable();
+    }, [currentId, setShowTable])
+
     const data = [
-    { id: 1, column1: 'Text1-1', column2: 'Text1-2', column3: 'Text1-3', column4: 'Text1-4' },
-    { id: 2, column1: 'Text2-1', column2: 'Text2-2', column3: 'Text2-3', column4: 'Text2-4' },
-    { id: 3, column1: 'Text3-1', column2: 'Text3-2', column3: 'Text3-3', column4: 'Text3-4' },
-    { id: 4, column1: 'Text4-1', column2: 'Text4-2', column3: 'Text4-3', column4: 'Text4-4' },
-    { id: 5, column1: 'Text5-1', column2: 'Text5-2', column3: 'Text5-3', column4: 'Text5-4' },
-    { id: 6, column1: 'Text6-1', column2: 'Text6-2', column3: 'Text6-3', column4: 'Text6-4' },
-    { id: 7, column1: 'Text7-1', column2: 'Text7-2', column3: 'Text7-3', column4: 'Text7-4' },
-    { id: 8, column1: 'Text8-1', column2: 'Text8-2', column3: 'Text8-3', column4: 'Text8-4' },
-    { id: 9, column1: 'Text9-1', column2: 'Text9-2', column3: 'Text9-3', column4: 'Text9-4' },
-    { id: 10, column1: 'Text10-1', column2: 'Text10-2', column3: 'Text10-3', column4: 'Text10-4' }
+        { id: 1, column1: 'Text1-1', column2: 'Text1-2', column3: 'Text1-3', column4: 'Text1-4' },
+        { id: 2, column1: 'Text2-1', column2: 'Text2-2', column3: 'Text2-3', column4: 'Text2-4' },
+        { id: 3, column1: 'Text3-1', column2: 'Text3-2', column3: 'Text3-3', column4: 'Text3-4' },
+        { id: 4, column1: 'Text4-1', column2: 'Text4-2', column3: 'Text4-3', column4: 'Text4-4' },
+        { id: 5, column1: 'Text5-1', column2: 'Text5-2', column3: 'Text5-3', column4: 'Text5-4' },
+        { id: 6, column1: 'Text6-1', column2: 'Text6-2', column3: 'Text6-3', column4: 'Text6-4' },
+        { id: 7, column1: 'Text7-1', column2: 'Text7-2', column3: 'Text7-3', column4: 'Text7-4' },
+        { id: 8, column1: 'Text8-1', column2: 'Text8-2', column3: 'Text8-3', column4: 'Text8-4' },
+        { id: 9, column1: 'Text9-1', column2: 'Text9-2', column3: 'Text9-3', column4: 'Text9-4' },
+        { id: 10, column1: 'Text10-1', column2: 'Text10-2', column3: 'Text10-3', column4: 'Text10-4' }
     ]
     const headers = ['Proposal Number', 'Title', 'PACMan Science Category', 'PACMan Probability', 'Original Science Category'];
     const csvContent =
-    headers.join(',') +
-    '\n' +
-    data.map((row) => Object.values(row).join(',')).join('\n');
+        headers.join(',') +
+        '\n' +
+        data.map((row) => Object.values(row).join(',')).join('\n');
 
     const encodedUri = encodeURI(`data:text/csv;charset=utf-8,${csvContent}`);
 
     const handleHighlight = (current_id) => {
-        setHighlighted((prevId) => (prevId === current_id ? prevId : current_id));
+        setHighlighted((prevId) => (prevId === current_id ? null : current_id));
     }
 
-  return (
-    <>
-        <div id="outer-container"className="container-fluid border border-1 border-black mt-5">
-            <div id="left-section" className=''>
-                <h6 className=''>All Proposals</h6>
-                <div className='table-container'>
-                    <table id="primary-table" className="">
-                        <thead className=''>
-                            <tr>
-                                <th scope="col">Proposal Number</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">PACMan Science Category</th>
-                                <th scope="col">PACMan Probability</th>
-                                <th scope="col">Original Science Category</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((row) => (
-                                <tr onClick={() => handleHighlight(row.id)} className={highlighted === row.id ? "highlighted" : ""} key={row.id}>
-                                    <td className="w-11 text-break" scope="row">{row.id}</td>
-                                    <td className="w-22 text-break">{row.column1}</td>
-                                    <td className="w-33 text-break">{row.column2}</td>
-                                    <td className='w-11 text-break'>{row.column3}</td>
-                                    <td className='w-22 text-break'>{row.column4}</td>
+    const viewLogs = () => {
+        setShowLogs(true)
+        setShowTable(false)
+    }
+
+    const anotherCycle = () => {
+        setShowTable(false)
+        setShowLogs(false)
+    }
+    return (
+        <>
+            <div id="outer-container" className="container-fluid border border-1 border-black mt-5">
+                <div className="col-md-9">
+                    <h6 className='mx-3 my-4 fw-bolder'>All Proposals</h6>
+                    <div className='table-container'>
+                        <table className='container-fluid'>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Proposal Number</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">PACMan Science Category</th>
+                                    <th scope="col">PACMan Probability</th>
+                                    <th scope="col">Original Science Category</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {dataToDisplay && dataToDisplay["proposal_table"] && Object.entries(dataToDisplay["proposal_table"]).map(([key, value]) => (
+                                    <tr onClick={() => handleHighlight(key)} className={highlighted === key ? "highlighted" : ""} key={key}>
+                                        <td className="col-2 text-break" scope="row">{key}</td> {/*Proposal Number*/}
+                                        <td className="col-4 text-break">{/*title*/}</td>
+                                        <td className="col-6 text-break">{value["PACMan Science Category"]}</td>
+                                        <td className="col-2 text-break">{value["PACMan Probability"]}</td>
+                                        <td className="col-4 text-break">{value["Original Science Category"]}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <h6 className='mx-3 my-4'>Alternate Categories</h6>
+                    {highlighted ?
+                    <div className='table-container rounded-4'>
+                        <table className="container-fluid">
+                            <thead>
+                                <tr>
+                                    <th scope="col">PACMan Science Category</th>
+                                    <th scope="col">PACMan Probability</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map((row) => (
+                                    <tr key={row.id}>
+                                        <td className="col-6 text-break">{row.column2}</td>
+                                        <td className='col-2 text-break'>{row.column3}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                        :
+                    (<AlternateCategoriesTest />)
+                    }
                 </div>
             </div>
-            <div id="right-section" className=''>
-                <h6 className=''>Alternate Categories</h6>
-                <div className='table-container'>
-                    <table className="">
-                        <thead>
-                            <tr>
-                                <th scope="col">PACMan Science Category</th>
-                                <th scope="col">PACMan Probability</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map((row) => (
-                                <tr key={row.id}>
-                                    <td className="w-33 text-break">{row.column2}</td>
-                                    <td className='w-11 text-break'>{row.column3}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+            <div className='button-tray container-fluid p-0'>
+                <button className='btn rounded-0' onClick={anotherCycle}>Categorize Another Cycle</button>
+                <a href={encodedUri} download='proposals.csv'>
+                    <button className='btn rounded-0'>Download As CSV</button>
+                </a>
+                <button className='btn rounded-0' onClick={viewLogs}>View Logs</button>
             </div>
-        </div>
-        <div className='button-tray container border'>
-            <button className='btn' onClick={handleClick}>Categorize Another Cycle</button>
-            <a href={encodedUri} download='proposals.csv'>
-                <button className='btn'>Download As CSV</button>
-            </a>
-            <button className='btn'>View Logs</button>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default ProposalTable;
