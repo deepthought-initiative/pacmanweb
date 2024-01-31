d623b289160a10bccc53954905e141a32ee2cf6b
AbhinavOhri <abhinavohri13@gmail.com>
2024-01-31 11:06:10 +0530

    used linter

--- frontend/src/components/util/Logs.jsx
+++ frontend/src/components/util/Logs.jsx
@@ -1,37 +1,43 @@
 /* eslint-disable react/prop-types */
 import "../../css/proposalTable.css";
 
-const Logs = ({data, setShowTable}) => {
-    const headers = ['Proposal Number', 'Title', 'PACMan Science Category', 'PACMan Probability', 'Original Science Category'];
-    const csvContent =
-    headers.join(',') +
-    '\n' +
-    data.map((row) => Object.values(row).join(',')).join('\n');
+const Logs = ({ data, setShowTable }) => {
+  const headers = [
+    "Proposal Number",
+    "Title",
+    "PACMan Science Category",
+    "PACMan Probability",
+    "Original Science Category",
+  ];
+  const csvContent =
+    headers.join(",") +
+    "\n" +
+    data.map((row) => Object.values(row).join(",")).join("\n");
 
-    const encodedUri = encodeURI(`data:text/csv;charset=utf-8,${csvContent}`);
-    const handleTable = async(event) => {
-        event.preventDefault();
-        setShowTable(true);
-    }
+  const encodedUri = encodeURI(`data:text/csv;charset=utf-8,${csvContent}`);
+  const handleTable = async (event) => {
+    event.preventDefault();
+    setShowTable(true);
+  };
 
   return (
-        <>
-        <div id="log-container" className="container-fluid mt-5">
-            {data.map((log, index) => (
-                <div key={index}>
-                    {log}
-                </div>
-            ))}
-        </div>
-        <div className='button-tray p-0 container-fluid'>
-            <button className='btn rounded-0' onClick={handleTable}>See Results</button>
-            <a href={encodedUri} download='proposals.csv'>
-                <button className='btn rounded-0'>Download As CSV</button>
-            </a>
-            <button className='btn rounded-0'>View Logs</button>
-        </div>
+    <>
+      <div id="log-container" className="container-fluid mt-5">
+        {data.map((log, index) => (
+          <div key={index}>{log}</div>
+        ))}
+      </div>
+      <div className="button-tray p-0 container-fluid">
+        <button className="btn rounded-0" onClick={handleTable}>
+          See Results
+        </button>
+        <a href={encodedUri} download="proposals.csv">
+          <button className="btn rounded-0">Download As CSV</button>
+        </a>
+        <button className="btn rounded-0">View Logs</button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Logs
+export default Logs;
