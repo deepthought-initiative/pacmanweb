1cf0ceb901e63b44063940fa0968fe73dd4bec60
Atharva Arya <aryaatharva18@gmail.com>
2024-02-05 19:15:43 +0530

    Add new api reoute to get available config options

--- pacmanweb/api/util.py
+++ pacmanweb/api/util.py
@@ -1,4 +1,5 @@
 import shutil
+from collections import defaultdict
 
 from pacmanweb import Config
 
@@ -10,3 +11,33 @@ def clean_previous_runs():
     for child in runs_dir.iterdir():
         if child.stem not in valid_names:
             shutil.rmtree(child)
+
+
+def cal_available_cycles(proposal_check=True, panelist_check=True):
+    pacman_path = Config.PACMAN_PATH
+    runs_dir = pacman_path / "runs"
+    proposal_directory = runs_dir / "input_proposal_data"
+    panelist_directory = runs_dir / "input_panelist_data"
+    result = {}
+
+    if proposal_directory.is_dir():
+        proposal_cycles = [item.stem for item in proposal_directory.iterdir() if item.is_dir()]
+        result["proposal_cycles"] = proposal_cycles
+    else:
+        result["proposal_cycles"] = f"No proposal directory found in f{str(proposal_directory)}"
+
+    if panelist_directory.is_dir():
+        panelist_cycles = [item.stem.split("_")[0] for item in panelist_directory.iterdir()]
+        result["panelist_cycles"] = panelist_cycles
+    else:
+        result["panelist_cycles"] = f"No panelist directory found in f{str(panelist_directory)}"
+        
+    models_dir = pacman_path / "models"
+    if models_dir.is_dir():
+        models = [item.name for item in models_dir.iterdir() if item.name.endswith(".joblib")]
+        result["models"] = models
+    else:
+        result["models"] = f"No models directory found in f{str(models_dir)}"
+
+    return result
+
