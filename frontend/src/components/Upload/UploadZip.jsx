1c8df6ebcdbcb95dcfb806e1b65e51d7706855fe
AbhinavOhri <abhinavohri13@gmail.com>
2024-02-07 00:37:40 +0530

    Update upload a zip page.

---
+++ frontend/src/components/Upload/UploadZip.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import "../../css/Upload.css";
+
+const UploadZipForm = () => {
+  const [zipFile, setZipFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const handleFileChange = (event) => {
+    setZipFile(event.target.files[0]);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!zipFile) {
+      alert("Please select a zip file");
+      return;
+    }
+
+    try {
+      setUploading(true);
+      const formData = new FormData();
+      formData.append("zipFile", zipFile);
+
+      const response = await fetch("http://example.com/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to upload file");
+      }
+
+      const data = await response.json();
+      console.log("File uploaded successfully:", data);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      alert("Failed to upload file");
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="instructions-container">
+        <ul className="instructions-list">
+          <li>The contents of the zip file:</li>
+          <li>
+            1. Proposal files need to be inside folders, the folder names being
+            the cycle names. The proposal folder name can be anything, just
+            needs to contain the word &quot;proposal&quot; (case sensitive).
+            <ul className="folder-structure">
+              Sample directory structure:
+              <li>
+                <span className="folder">-proposals (or some other name)</span>
+                <ul>
+                  <li>
+                    <span className="folder">---221023</span>
+                    <ul>
+                      <li>
+                        <span className="file">------00001.txtx</span>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span className="folder">---221212</span>
+                    <ul>
+                      <li>
+                        <span className="file">------00001.txtx</span>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            2. All files ending either with joblib or npy would move to the
+            models folder.
+          </li>
+          <li>
+            3. All files ending with _panelist.csv will be moved to the panelist
+            folder.
+          </li>
+        </ul>
+      </div>
+      <form className="button-tray" onSubmit={handleFormSubmit}>
+        <input type="file" onChange={handleFileChange} />
+        <button className="btn rounded-1" type="submit" disabled={uploading}>
+          {uploading ? "Uploading..." : "Upload"}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default UploadZipForm;
