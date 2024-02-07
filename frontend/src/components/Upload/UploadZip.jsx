775f6fc97b8bce81a3d4e06e65443f87eea1d9e7
AbhinavOhri <abhinavohri13@gmail.com>
2024-02-07 11:46:07 +0530

    Uploading a file functionality

--- frontend/src/components/Upload/UploadZip.jsx
+++ frontend/src/components/Upload/UploadZip.jsx
@@ -21,7 +21,7 @@ const UploadZipForm = () => {
 
     try {
       const formData = new FormData();
-      formData.append("zipFile", zipFile);
+      formData.append("file", zipFile);
 
       const response = await fetch(
         "http://127.0.0.1:5000/api/upload?api_key=barebones",
