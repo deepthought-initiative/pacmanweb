6a9c121a0a1408325e40b50a7a46106ec28d2074
AbhinavOhri <abhinavohri13@gmail.com>
2024-03-05 01:12:53 +0530

    Implementing download functionality

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ import { defineConfig } from "vite";
 // });
 
 // in prod uncomment this
-const { REACT_APP_BASE_URL, REACT_APP_API_URL } = process.env;
+// const { REACT_APP_BASE_URL, REACT_APP_API_URL } = process.env;
 
 // in dev uncomment this
-// const REACT_APP_BASE_URL="http://127.0.0.1:8080";
-// const REACT_APP_API_URL="http://127.0.0.1:8000";
+const REACT_APP_BASE_URL = "http://127.0.0.1:8080";
+const REACT_APP_API_URL = "http://127.0.0.1:8000";
 
 export default defineConfig({
   base: "/",
