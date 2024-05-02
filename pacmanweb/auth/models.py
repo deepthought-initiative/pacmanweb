99d20e2c59e393ea8bb664ca589cb4d86758b241
Atharva Arya <55894364+atharva-2001@users.noreply.github.com>
2024-05-02 12:10:29 +0530

    check for encrypted passwords (#67)

--- pacmanweb/auth/models.py
+++ pacmanweb/auth/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from pacmanweb import Config
+from werkzeug.security import generate_password_hash, check_password_hash
 
 credentials = Config.USERS
 
@@ -12,6 +13,7 @@ class User(UserMixin):
 
     @staticmethod
     def get(username, password):
-        if username in credentials.keys() and credentials[username] == password:
+        check_pswd = check_password_hash(credentials[username], password)
+        if username in credentials.keys() and check_pswd:
             return User(username)
         return None
