b439b4196a2b55aba0a0a42db1b218964c795563
AbhinavOhri <abhinavohri13@gmail.com>
2024-06-06 15:53:31 +0530

    Creating list of panelists from user input

--- frontend/src/components/util/TextArea.jsx
+++ frontend/src/components/util/TextArea.jsx
@@ -1,18 +1,28 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
-const TextArea = ({ value, setValue }) => {
+const TextArea = ({ setValue }) => {
   const handleOnChange = (event) => {
-    console.log(value);
-    setValue(event.target.value);
+    setTextEntered(event.target.value);
+    setTextEntered(event.target.value);
+
+    let listOfPanelists = textEntered
+      .trim()
+      .split(/\n+/)
+      .map((panelist) => panelist.trim());
+
+    console.log(listOfPanelists);
+    setValue(listOfPanelists);
   };
+  const [textEntered, setTextEntered] = useState("");
   return (
     <div>
       <InputGroup>
         <Form.Control
           as="textarea"
-          value={value}
+          value={textEntered}
           aria-label="With textarea"
           onChange={handleOnChange}
         />
