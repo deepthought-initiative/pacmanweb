abb5b2899850c611e3a04f47dd35678d1451cdc3
AbhinavOhri <abhinavohri13@gmail.com>
2024-06-24 14:32:08 +0530

    Add sample data and edit and delete images

--- frontend/src/components/AdminDashboard/Dashboard.jsx
+++ frontend/src/components/AdminDashboard/Dashboard.jsx
@@ -1,9 +1,44 @@
 import Table from "react-bootstrap/Table";
+import UserDelete from "../../assets/UserDelete.png";
+import UserEdit from "../../assets/UserEdit.png";
 
 const Dashboard = () => {
+  const users = [
+    {
+      UID: "1",
+      Username: "user1",
+      Password: "password1",
+      Status: "normal",
+    },
+    {
+      UID: "2",
+      Username: "user2",
+      Password: "password2",
+      Status: "admin",
+    },
+    {
+      UID: "3",
+      Username: "user3",
+      Password: "password3",
+      Status: "normal",
+    },
+    {
+      UID: "4",
+      Username: "user4",
+      Password: "password4",
+      Status: "admin",
+    },
+    {
+      UID: "5",
+      Username: "user5",
+      Password: "password5",
+      Status: "normal",
+    },
+  ];
   return (
     <div className="user-list-container">
       <h2>All Users</h2>
+      <div className="row"></div>
       <div>
         <Table striped bordered hover>
           <thead>
@@ -16,20 +51,20 @@ const Dashboard = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>@mdo</td>
-            </tr>
+            {users.map((user, index) => (
+              <tr key={user["UID"]}>
+                <td>{index}</td>
+                <td>{user["Username"]}</td>
+                <td>{user["Password"]}</td>
+                <td>{user["Status"]}</td>
+                <td>
+                  <div className="user-edit-options">
+                    <img src={UserEdit} />
+                    <img src={UserDelete} />
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       </div>
