6eaf2ed7bd9c5d12f0da0965a72705053488e515
Atharva Arya <aryaatharva18@gmail.com>
2024-06-27 17:53:01 +0530

    Move to redis

--- pacmanweb/api/admin.py
+++ pacmanweb/api/admin.py
@@ -5,26 +5,11 @@ from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash
 
+import redis
 from pacmanweb import Config
+redis_instance = redis.from_url(Config.CELERY_RESULT_BACKEND)
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
-
-
-# separate class because resources won't be opened/closed that often
-class UpdateSecrets:
-    def __init__(self):
-        ROOTDIR = Config().ROOTDIR
-        self.secrets_fpath = ROOTDIR.parent / "secrets.json"
-
-    def read_secrets(self):
-        with open(self.secrets_fpath, "r") as secrets:
-            secrets_data = json.load(secrets)
-        return secrets_data
-
-    def update_secrets(self, new_secrets):
-        with open(self.secrets_fpath, "w") as secrets_file:
-            json.dump(new_secrets, secrets_file)
-
 
 def admin_only(f):
     @wraps(f)
@@ -39,17 +24,16 @@ def admin_only(f):
 
 @admin_bp.route("/ifexists", methods=["GET"])
 def exists():
-    secrets = UpdateSecrets()
-    secrets_file = secrets.read_secrets()
     username = request.form.get("username", None)
     
-    if username is None or username not in secrets_file["users"].keys():
+    if username is None or f'user_{username}'.encode('utf-8') not in redis_instance.keys('*'):
         return {'value': 'User not found'}, 404
-
-    if username not in secrets_file["admins"]:
-        return {'value': 'User is not admin but exists'}, 404
     else:
-        return {'value': 'User is admin and exists'}, 200
+        user_data = redis_instance.hgetall(f"user_{username}")
+        if user_data[b"admin"] == b"True":
+            return {'value': 'User is admin and exists'}, 200
+        else:
+            return {'value': 'User is not admin but exists'}, 404
 
 
 
@@ -57,11 +41,12 @@ def exists():
 @login_required
 @admin_only
 def register_update_user():
-    secrets = UpdateSecrets()
-    secrets_file = secrets.read_secrets()
     username = request.form["username"]
+    newuserdata = {
+            "username": username,
+    }
 
-    if username not in secrets_file["users"].keys():
+    if f'user_{username}'.encode('utf-8') not in redis_instance.keys('*'):
         # this means adding a new user
         if not (
             request.form.get("password", None)
@@ -70,41 +55,38 @@ def register_update_user():
             return {
                 "value": "New user detected. Please send keys- password, isadmin if adding a new user."
             }, 401
+        userdata = {}
+    else:
+        # user already exists, check for overwrite key
+        if request.form.get("overwrite", None) not in ["True", "true"]:
+            return {
+            "value": "This user is already there, pass an overwrite key if you wish to overwrite."
+        }, 401
+        userdata = redis_instance.hgetall(f"user_{username}")
+        userdata = {key.decode('utf-8'): value.decode('utf-8') for key, value in userdata.items()}
 
-
-    # edit password
+    # edit password and admin status
     password = request.form.get("password", None)
     if password:
-        # if user already there, ask for confirmation
-        if username in secrets_file["users"].keys():
-            # user already there, check for overwrite key
-            if request.form.get("overwrite", None) not in ["True", "true"]:
-                return {
-                "value": "This user is already there, pass an overwrite key if you wish to overwrite."
-            }, 401
-            
         encoded_pass = generate_password_hash(password, method="pbkdf2:sha256")
-        secrets_file["users"][username] = encoded_pass
-        
+        newuserdata["password"] = encoded_pass
+    
+    
+
     # lookout for bad isadmin values
     isadmin = request.form.get("isadmin", None)
-    if isadmin in ["true", "True"]:
-        isadmin = True
-    elif isadmin in ["false", "False"] or isadmin is None:
-        isadmin = False
-    else:
-        return {"value": "isadmin can only be True or False"}, 401
+    if isadmin:
+        if isadmin in ["true", "True"]:
+            isadmin = True
+        elif isadmin in ["false", "False"] or isadmin is None:
+            isadmin = False
+        else:
+            return {"value": "isadmin can only be True or False"}, 401
+        newuserdata["admin"] = str(isadmin)
 
-    if isadmin and username not in secrets_file["admins"]:
-        secrets_file["admins"].append(username)
-    elif not isadmin and username in secrets_file["admins"]:
-        secrets_file["admins"].remove(username)
-    else:
-        # is admin and in admins list pass
-        # is not admin and not in admins list pass
-        pass
-
-    secrets.update_secrets(secrets_file)
+    # newuserdata overwrites userdata in case of conflict
+    userdata = userdata | newuserdata
+    redis_instance.hset(f'user_{username}', mapping=userdata)
     return {"value": f"New user {username} added/updated."}, 201
 
 
@@ -113,17 +95,10 @@ def register_update_user():
 @admin_only
 def delete_user():
     username = request.form["username"]
-    secrets = UpdateSecrets()
-    secrets_data = secrets.read_secrets()
-
-    if username in secrets_data["users"].keys():
-        del secrets_data["users"][username]
+    if f'user_{username}'.encode('utf-8') in redis_instance.keys('*'):
+        redis_instance.delete(f'user_{username}'.encode('utf-8'))
     else:
         return {"value": "Username not found"}, 401
-
-    if username in secrets_data["admins"]:
-        secrets_data["admins"].remove(username)
-    secrets.update_secrets(secrets_data)
     return {"value": f"User {username} deleted."}, 200
 
 
@@ -131,14 +106,11 @@ def delete_user():
 @login_required
 @admin_only
 def return_user_data():
-    secrets = UpdateSecrets()
-    secrets_data = secrets.read_secrets()
-    users = secrets_data["users"].keys()
-    admins = secrets_data["admins"]
-
-    res_total = [
-        {"username": item, "isadmin": item in admins}
-        for item in users
-    ]
-
-    return jsonify(res_total)
+    result = []
+    for key in redis_instance.keys("user_*"):
+        user_data = redis_instance.hgetall(key)
+        result.append({
+            "username": user_data[b"username"].decode('utf-8'),
+            "isadmin": user_data[b"admin"].decode('utf-8')
+        })
+    return result
