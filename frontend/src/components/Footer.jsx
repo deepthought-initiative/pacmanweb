6568bb732ca553036f4ae444b336e2ea5262ac97
AbhinavOhri <abhinavohri13@gmail.com>
2024-06-28 12:50:25 +0530

    Add footer with attributions

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+// Footer.jsx
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">
+          <a href="https://www.flaticon.com/free-icons/edit" title="edit icons">
+            Edit icons created by Kiranshastry - Flaticon
+          </a>
+          |
+          <a
+            href="https://www.flaticon.com/free-icons/delete"
+            title="delete icons"
+          >
+            Delete icons created by Kiranshastry - Flaticon
+          </a>
+          |
+          <a href="https://www.flaticon.com/free-icons/user" title="user icons">
+            User icons created by Freepik - Flaticon
+          </a>
+          |
+          <a href="https://www.flaticon.com/free-icons/red" title="red icons">
+            Red icons created by hqrloveq - Flaticon
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./App.css";
 import App from "./App.jsx";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>
 );
