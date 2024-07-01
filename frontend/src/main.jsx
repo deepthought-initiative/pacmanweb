c694b15aa10a3af49e1c2ac2a6d4268467deb397
AbhinavOhri <abhinavohri13@gmail.com>
2024-07-01 12:50:33 +0530

    Removing footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 import App from "./App.jsx";
-import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Footer />
+    {/* <Footer /> */}
   </React.StrictMode>
 );
