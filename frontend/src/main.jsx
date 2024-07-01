a56069550a2efdcc1749c2b6f6733fbbbbfe0e83
AbhinavOhri <abhinavohri13@gmail.com>
2024-07-01 12:50:33 +0530

    Removing footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthContextProvider>
       <BrowserRouter>
       <App />
-      <Footer />
+      {/* <Footer /> */}
       </BrowserRouter>
       </AuthContextProvider>
   </React.StrictMode>
