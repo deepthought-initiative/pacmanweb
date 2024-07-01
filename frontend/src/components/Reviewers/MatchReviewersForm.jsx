0a288e3658b643089b2cf9d198c3793fef6e1249
AbhinavOhri <abhinavohri13@gmail.com>
2024-07-01 12:51:28 +0530

    Removing redudnant url code

--- frontend/src/components/Reviewers/MatchReviewersForm.jsx
+++ frontend/src/components/Reviewers/MatchReviewersForm.jsx
@@ -265,19 +265,32 @@ const MatchReviewersForm = ({
     resetErrors();
     console.log(panelistNames);
     const checkErrors = validateFields();
+    const params = [
+      `mode=${mode}`,
+      `main_test_cycle=${currentCycle}`,
+      `modelfile=${selectedModal}`,
+      `assignment_number_top_reviewers=${numberOfTopReviewers}`,
+      `close_collaborator_time_frame=${closeCollaboratorTimeFrame}`,
+      `log_level=${logLevel}`,
+      `assignment_number_top_reviewers=${numberOfTopReviewers}`,
+    ];
+    if (panelistNames.length !== 0) {
+      params.push("panelist_names", panelistNames);
+      params.push("panelist_names_mode", "append");
+    }
+
+    const query = params.join("&");
+    const url = `/api/run_pacman?${query}`;
     if (checkErrors) {
       let spawnResponse;
       setLoading(true);
-      spawnResponse = await fetch(
-        `/api/run_pacman?mode=${mode}&main_test_cycle=${currentCycle}&modelfile=${selectedModal}&assignment_number_top_reviewers=${numberOfTopReviewers}&close_collaborator_time_frame=${closeCollaboratorTimeFrame}&log_level=${logLevel}&panelist_names=${panelistNames}&panelist_names_mode=append`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: "Basic " + btoa("default:barebones"),
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      spawnResponse = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: "Basic " + btoa("default:barebones"),
+          "Content-Type": "application/json",
+        },
+      });
       if (spawnResponse.status === 429) {
         setModalShow(true);
       } else {
