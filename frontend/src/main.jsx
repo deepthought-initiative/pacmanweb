5bfc5e85045a7d370ccc4cab167193d4ced8fd3b
Abhinav Ohri <abhinavohri13@gmail.com>
2024-07-05 12:37:17 +0530

    Shifting router above app in heirarchy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,16 @@ import "./App.css";
 import App from "./App.jsx";
 import Footer from "./components/Footer.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter} from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <AuthContextProvider>
+      <BrowserRouter>
       <App />
       <Footer />
+      </BrowserRouter>
       </AuthContextProvider>
   </React.StrictMode>
 );
