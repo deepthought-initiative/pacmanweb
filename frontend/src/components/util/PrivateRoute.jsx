2cfac596d8743b7f42299f75d5750e02b3c04829
Abhinav Ohri <abhinavohri13@gmail.com>
2024-07-05 12:38:37 +0530

    Fixing private route

--- frontend/src/components/util/PrivateRoute.jsx
+++ frontend/src/components/util/PrivateRoute.jsx
@@ -1,33 +1,36 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
-  const [status, setStatus] = useState(false);
-  const navigate = useNavigate();
+  const { isLoggedIn } = useContext(AuthContext);
 
-  useEffect(() => {
-    const checkLogin = async () => {
-      try {
-        const response = await fetch("/api/logged_in");
-        const data = await response.json();
-        const s = data.value === "True";
-        console.log(s);
-        setStatus(s);
-        if (!s) {
-          navigate("/login");
-        }
-      } catch (error) {
-        console.error("Error checking login status:", error);
-        setStatus(false);
-        navigate("/login");
-      }
-    };
-    checkLogin();
-  }, [navigate]);
+  // useEffect(() => {
+  //   let loggedIn = localStorage.getItem("loggedIn");
+  //   const checkLogin = async () => {
+  //     try {
+  //       const response = await fetch("/api/logged_in");
+  //       const data = await response.json();
+  //       const s = data.value === "True";
+  //       console.log(s);
+  //       setStatus(s);
+  //       if (!s || !loggedIn) {
+  //         localStorage.removeItem("username");
+  //         localStorage.removeItem("isUserAdmin");
+  //         console.log("hi");
+  //         navigate("/login");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error checking login status:", error);
+  //       setStatus(false);
+  //       navigate("/login");
+  //     }
+  //   };
+  //   checkLogin();
+  // }, [navigate]);
 
-  return status ? children : null;
+  return isLoggedIn ? children : null;
 };
 
 export default PrivateRoute;
