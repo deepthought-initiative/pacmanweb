5af12aafaf0430d0fa3b64fe2fbdbd1286104fe5
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-08-27 16:43:53 +0530

    Add test to check submit button

--- frontend/tests/CategorizationForm.test.jsx
+++ frontend/tests/CategorizationForm.test.jsx
@@ -1,35 +1,77 @@
-import { describe, it, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { it, expect, vi, afterEach, afterAll, beforeAll } from 'vitest';
+import { render, screen,fireEvent, cleanup, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+import '@testing-library/jest-dom/vitest';
 import CategorizationForm from "../src/components/ProposalCategorization/CategorizationForm.jsx";
 
-describe('CategorizationForm', () => {
-  it('should render initial state correctly', () => {
-    // Mock the required props
-    const mockProps = {
-      allCycles: [],
-      modalFile: [],
-      mode: 'PROP',
-      renderTableComponent: vi.fn(),
-      button_label: 'Submit'
-    };
+afterEach(cleanup);
 
-    render(<CategorizationForm {...mockProps} />);
+const server = setupServer(
+  http.get('/api/run_pacman', () => {
+    window.console.log("mock server");
+    return HttpResponse.json({ "output": "PACMan running with task id 4c9cd774-6180-4df9-930c-96cf664d0793", "result_id": "4c9cd774-6180-4df9-930c-96cf664d0793" }
+    )
+  }),
+)
 
-    expect(screen.getByText('Start a new process')).toBeDefined();
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
-    // Check that the cycle selection container is present
-    expect(screen.queryByTestId('cycle-selection-container')).toBeDefined();
+it('should render initial state correctly', () => {
+  const mockProps = {
+    allCycles: [{
+      cycleNumber: "221026",
+      label: "2221026",
+      style: {
+        backgroundColor: ""
+      }
+    }],
+    modalFile: [],
+    mode: 'PROP',
+    renderTableComponent: vi.fn(),
+    button_label: 'Submit'
+  };
 
-    // Check that the config options are present
-    expect(screen.queryByTestId('config-options-container')).toBeDefined();
+  render(<CategorizationForm {...mockProps} />);
+  expect(screen.getByText('Start a new process')).toBeDefined();
+  expect(screen.queryByTestId('cycle-selection-container')).toBeDefined();
+  expect(screen.queryByTestId('config-options-container')).toBeDefined();
+  expect(screen.queryByTestId('logs-container')).not.toBeTruthy();
+  expect(screen.queryByTestId('table-container')).toBeNull();
+  expect(mockProps.renderTableComponent).not.toHaveBeenCalled();
+});
 
-    // Check that the logs container is not present
-    expect(screen.queryByTestId('logs-container')).toBeNull();
+it('should show logs and hide table when submit button is clicked', async () => {
+  const mockProps = {
+    allCycles: [{
+      cycleNumber: "221026",
+      label: "2221026",
+      style: {
+        backgroundColor: ""
+      }
+    }],
+    modalFile: ["strolger_pacman_model_7cycles.joblib"],
+    mode: 'PROP',
+    renderTableComponent: vi.fn(),
+    button_label: 'Submit',
+    handleClick: vi.fn(),
+    loading: false,
+  };
+  const cycleNumber = "221026";
+  render(<CategorizationForm {...mockProps} />);
+  const cycleDropdown = screen.getByTestId("dropdown-Selected Current Cycle");
+  await userEvent.click(cycleDropdown);
+  const cycleOption = screen.getByTestId(`dropdown-cycle-option-${cycleNumber}`);
+  await userEvent.click(cycleOption);
+  const submitButton = screen.getByTestId('submit-button');
+  await userEvent.click(submitButton);
 
-    // Check that the table container is not present
-    expect(screen.queryByTestId('table-container')).toBeNull();
-
-    // Verify that renderTableComponent was not called
-    expect(mockProps.renderTableComponent).not.toHaveBeenCalled();
+  await waitFor(() => {
+    expect(screen.queryByTestId('logs-container')).toBeInTheDocument();
+    expect(screen.queryByTestId('table-container')).not.toBeInTheDocument();
   });
+
 });
