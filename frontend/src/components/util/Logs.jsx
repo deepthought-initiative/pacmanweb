edbb76521f0824453015ebb592653627fa9a28ca
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-08-28 15:53:31 +0530

    Move download a file functionality to a new file

--- frontend/src/components/util/Logs.jsx
+++ frontend/src/components/util/Logs.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
