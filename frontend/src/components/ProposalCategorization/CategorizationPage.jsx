45062c079b36c6ce4492ad2dfdba73bab1b9e533
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-08-29 10:48:14 +0530

    Create a separate page for categorization

---
+++ frontend/src/components/ProposalCategorization/CategorizationPage.jsx
@@ -0,0 +1,333 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+import { useCallback, useEffect, useRef, useState } from "react";
+import "../../css/searchBox.css";
+import Logs from "../util/Logs.jsx";
+import NewDropdown from "../util/NewDropdown.jsx";
+import OtherConfigOptionsCategorize from "../util/OtherConfigOptionsCategorize.jsx";
+
+const CategorizationPage = ({
+  allCycles,
+  modalFile,
+  mode,
+  renderTableComponent,
+  button_label,
+}) => {
+  const [modalShow, setModalShow] = useState(false); // for showing alert when running multiple processes at the same time
+  const [showTable, setShowTable] = useState(false);
+  const [showLogs, setShowLogs] = useState(false);
+  const [logs, setLogs] = useState([]);
+  const [currentId, setCurrentId] = useState();
+  const [showTerminateProcess, setShowTerminateProcess] = useState(true);
+  const [currentCycle, setCurrentCycle] = useState();
+  const [progressPercentage, setProgressPercentage] = useState(0);
+
+  // state variables for other config options
+  const [runName, setRunName] = useState("");
+  const [selectedModal, setSelectedModal] = useState(
+    "strolger_pacman_model_7cycles.joblib"
+  );
+  const [logLevel, setLogLevel] = useState("info");
+
+  // Error variables
+  const [currentCycleError, setCurrentCycleError] = useState("");
+  const [selectedModalError, setSelectedModalError] = useState("");
+  const [logLevelError, setLogLevelError] = useState("");
+  //
+  const [dataToDisplay, setDataToDisplay] = useState([]);
+  const [processStatus, setProcessStatus] = useState();
+  const logContainerRef = useRef(null);
+
+  //loading
+  const [loading, setLoading] = useState(false);
+
+  // Text description for alert modals
+  const multipleRequestAlertTitle = "Process Running Elsewhere";
+  const multipleRequestAlertDesc =
+    "It seems you started a process somewhere else. You can move to that tab or start a process here after terminating the process.";
+
+  const terminateAllProcesses = useCallback(async () => {
+    if (!currentId) {
+      return;
+    }
+    await fetch(`/api/terminate/${currentId}?mode=${mode}`, {
+      method: "POST",
+    });
+    onTerminate();
+  }, [currentId, mode]);
+
+  useEffect(() => {
+    const handleBeforeUnload = async (event) => {
+      await terminateAllProcesses();
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [allCycles, terminateAllProcesses]);
+
+  const validateFields = () => {
+    let noError = true;
+    if (!currentCycle) {
+      setCurrentCycleError("Required");
+      noError = false;
+    }
+    if (!selectedModal) {
+      setSelectedModalError("Required");
+      noError = false;
+    }
+    if (!logLevel) {
+      setLogLevelError("Required");
+      noError = false;
+    }
+    return noError;
+  };
+
+  const resetErrors = () => {
+    setCurrentCycleError("");
+    setSelectedModalError("");
+    setLogLevelError("");
+  };
+
+  const onTerminate = () => {
+    setCurrentId();
+    setLogLevel("info");
+    setShowLogs(false);
+    setShowTable(false);
+    setProgressPercentage(0);
+    setLogs([]);
+    setShowTerminateProcess(true);
+    setProcessStatus();
+    setCurrentCycle("");
+    setRunName("");
+    setSelectedModal("strolger_pacman_model_7cycles.joblib");
+    setLoading(false);
+  };
+
+  const handleFilteringCycles = (newCurrentCycle) => {
+    const newCycles = allCycles.filter((cycle) => {
+      return cycle.cycleNumber !== newCurrentCycle;
+    });
+    setCurrentCycle(newCurrentCycle);
+  };
+
+  const fetchTable = useCallback(
+    async (curId) => {
+      if (!curId) {
+        return;
+      }
+      let tableCategory = "";
+      if (mode == "PROP") {
+        tableCategory = "proposal_cat_output";
+      }
+      try {
+        const tableResponse = await fetch(
+          `/api/outputs/${tableCategory}/${curId}?cycle_number=${currentCycle}`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: { Authorization: "Basic " + btoa("default:barebones") },
+          }
+        );
+        if (!tableResponse.ok) {
+          setProgressPercentage(100);
+          throw new Error(
+            `Failed to fetch table data: ${tableResponse.statusText}`
+          );
+        }
+        const tableData = await tableResponse.json();
+        const [tabularData, code] = tableData;
+        setDataToDisplay(tabularData);
+        setProcessStatus(code);
+        if (code === 200) {
+          setProgressPercentage(100);
+          setLogs((prevLogs) => [...prevLogs, "PROCESS SUCCESSFUL"]);
+        } else if (code === 204) {
+          setProgressPercentage(100);
+          setLogs((prevLogs) => [...prevLogs, "DUPLICATION FILE IS EMPTY."]);
+        } else {
+          setProgressPercentage(100);
+          alert("Process failed! Please try again");
+          setLogs((prevLogs) => [...prevLogs, "PROCESS FAILED"]);
+        }
+        logContainerRef.current.scrollTop =
+          logContainerRef.current.scrollHeight;
+        console.log(processStatus);
+      } catch (error) {
+        setProgressPercentage(100);
+        console.error("Error fetching table data:", error);
+      }
+    },
+    [currentCycle, mode, processStatus]
+  );
+
+  const startFetchingLogs = useCallback(
+    async (curId) => {
+      let reconnectFrequencySeconds = 1;
+
+      const fetchLogs = async () => {
+        try {
+          const eventSource = new EventSource(`/api/stream/${curId}`);
+          eventSource.onopen = () => {
+            setShowTerminateProcess(true);
+          };
+          eventSource.onmessage = async (event) => {
+            const newLog = event.data;
+            if (newLog.includes("STARTING RUN")) {
+              setProgressPercentage(10);
+            }
+            if (newLog.includes("Log file can be found")) {
+              setProgressPercentage(50);
+            }
+            if (
+              newLog.includes("PROCESS COMPLETE") ||
+              newLog.includes("run complete")
+            ) {
+              await fetchTable(curId);
+              eventSource.close();
+              setShowTerminateProcess(false);
+              return; // Exit after process completion
+            }
+            setLogs((prevLogs) => [...prevLogs, newLog]);
+            logContainerRef.current.scrollTop =
+              logContainerRef.current.scrollHeight;
+          };
+          eventSource.onerror = (error) => {
+            console.error("EventSource failed:", error);
+            setShowTerminateProcess(false);
+            reconnectFrequencySeconds = Math.min(
+              reconnectFrequencySeconds * 2,
+              64
+            );
+            setTimeout(
+              () => startFetchingLogs(curId),
+              reconnectFrequencySeconds * 1000
+            );
+          };
+        } catch (error) {
+          console.error("Error fetching logs:", error);
+          setTimeout(
+            () => startFetchingLogs(curId),
+            reconnectFrequencySeconds * 1000
+          );
+        }
+      };
+
+      await fetchLogs();
+    },
+    [fetchTable, setShowTerminateProcess, setLogs, logContainerRef]
+  );
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+    resetErrors();
+    const checkErrors = validateFields();
+    if (checkErrors) {
+      let spawnResponse;
+      setLoading(true);
+      spawnResponse = await fetch(
+        `/api/run_pacman?mode=${mode}&main_test_cycle=${currentCycle}&modelfile=${selectedModal}&log_level=${logLevel}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "Basic " + btoa("default:barebones"),
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (spawnResponse.status === 429) {
+        setModalShow(true);
+      } else {
+        const data = await spawnResponse.json();
+        setCurrentId(data["result_id"]);
+        setShowLogs(true);
+        setLoading(false);
+        await startFetchingLogs(data["result_id"]);
+      }
+    }
+  };
+
+  const preventClick = (event) => {
+    event.preventDefault();
+    return false;
+  };
+
+  return (
+    <div className="mt-5" id="main-container">
+      {!showLogs && !showTable && <h3>Start a new process</h3>}
+      <div className={`${mode === "DUP" && "d-flex"}`}>
+        <div className={`row ${mode === "DUP" && "col-md-6"}`}>
+          <NewDropdown
+            data={allCycles}
+            label="Selected Current Cycle"
+            desc="Prefix used throughout script to match with cycle description"
+            inputField={currentCycle}
+            multiple={false}
+            setInputField={handleFilteringCycles}
+            disabled={showTable || showLogs}
+            error={currentCycleError}
+          />
+        </div>
+      </div>
+      {showTable ? (
+        renderTableComponent({
+          currentId: currentId,
+          currentCycle: currentCycle,
+          setShowTable: setShowTable,
+          showLogs: showLogs,
+          showTable: showTable,
+          setShowLogs: setShowLogs,
+          onCategorizeAnotherCycle: onTerminate,
+          dataToDisplay: dataToDisplay,
+          mode: mode,
+        })
+      ) : showLogs ? (
+        <Logs
+          currentId={currentId}
+          currentCycle={currentCycle}
+          setShowTable={setShowTable}
+          terminateAllProcesses={terminateAllProcesses}
+          onTerminate={onTerminate}
+          logs={logs}
+          mode={mode}
+          setShowLogs={setShowLogs}
+          showLogs={showLogs}
+          showTable={showTable}
+          preventClick={preventClick}
+          loading={loading}
+          progressPercentage={progressPercentage}
+          processStatus={processStatus}
+          logContainerRef={logContainerRef}
+          showTerminateProcess={showTerminateProcess}
+          dataToDisplay={dataToDisplay}
+        />
+      ) : (
+        <OtherConfigOptionsCategorize
+          button_label={button_label}
+          logLevelError={logLevelError}
+          modalShow={modalShow}
+          multipleRequestAlertTitle={multipleRequestAlertTitle}
+          multipleRequestAlertDesc={multipleRequestAlertDesc}
+          setModalShow={setModalShow}
+          handleClick={handleClick}
+          preventClick={preventClick}
+          currentCycle={currentCycle}
+          runName={runName}
+          modalFile={modalFile}
+          logLevel={logLevel}
+          setLogLevel={setLogLevel}
+          selectedModal={selectedModal}
+          setSelectedModal={setSelectedModal}
+          setRunName={setRunName}
+          selectedModalError={selectedModalError}
+          loading={loading}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CategorizationPage;
