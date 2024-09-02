bdd0cf7e60d9dc96363fe42e52076bc3e85fbe2a
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-09-02 10:02:54 +0530

    use escape sequences for apostrophes

--- frontend/src/components/util/PageNotFound.jsx
+++ frontend/src/components/util/PageNotFound.jsx
@@ -4,7 +4,7 @@ const PageNotFound = () => {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
+      <p>The page you&apos;re looking for doesn&apos;t exist.</p>
       <Link to="/">Go back to the home page</Link>
     </div>
   );
