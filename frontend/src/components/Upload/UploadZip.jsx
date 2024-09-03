bb767f01531adb02cd0f24ff1d388a31fd3a066c
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-09-04 02:28:49 +0530

    Add custom toasts to upload paged

--- frontend/src/components/Upload/UploadZip.jsx
+++ frontend/src/components/Upload/UploadZip.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import "../../css/Upload.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import AppContext from "../../context/AppContext";
+import CustomToast from "../util/CustomToast";
 
 const UploadZipForm = () => {
   const [zipFile, setZipFile] = useState(null);
   const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState("");
+
+  const { showToast, setShowToast, toastVariant, setToastVariant } =
+    useContext(AppContext);
 
   const handleFileChange = (event) => {
     setZipFile(event.target.files[0]);
@@ -11,32 +19,30 @@ const UploadZipForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     if (!zipFile) {
-      alert("Please select a zip file");
+      setUploadError("Please select a zip file");
       return;
     }
-
     setUploading(true);
-
     try {
       const formData = new FormData();
       formData.append("file", zipFile);
-
       const response = await fetch(`/api/upload`, {
         method: "POST",
         body: formData,
       });
-
       if (!response.ok) {
         throw new Error("Failed to upload file");
       }
-
       const message = await response.json();
-      alert(message["response"]);
+      setUploadError(message["response"]);
+      setShowToast(true);
+      setToastVariant("success");
     } catch (error) {
       console.error("Error uploading file:", error);
-      alert("Failed to upload file");
+      setUploadError("Failed to upload file");
+      setShowToast(true);
+      setToastVariant("danger");
     } finally {
       setUploading(false);
     }
@@ -44,6 +50,19 @@ const UploadZipForm = () => {
 
   return (
     <>
+      {showToast && (
+        <CustomToast
+          showToast={showToast}
+          setShowToast={setShowToast}
+          variant={toastVariant}
+          style={{
+            position: "absolute",
+            top: "10px",
+            left: "10px",
+            backgroundColor: "transparent",
+          }}
+        />
+      )}
       <div className="instructions-container">
         <ul className="instructions-list">
           <li>The contents of the zip file:</li>
@@ -86,12 +105,19 @@ const UploadZipForm = () => {
           </li>
         </ul>
       </div>
-      <form className="button-tray" onSubmit={handleFormSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button className="btn rounded-1" type="submit" disabled={uploading}>
-          {uploading ? "Uploading..." : "Upload"}
-        </button>
-      </form>
+      <Form className="button-tray" onSubmit={handleFormSubmit}>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Control onChange={handleFileChange} type="file" />
+          {uploadError && (
+            <Form.Control.Feedback type="invalid">
+              {uploadError}
+            </Form.Control.Feedback>
+          )}
+        </Form.Group>
+        <Button className="btn rounded-1" disabled={uploading} type="submit">
+          {uploading ? "Uploading..." : "Upload File"}
+        </Button>
+      </Form>
     </>
   );
 };
