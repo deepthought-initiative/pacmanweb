07ede64087db38efd41493121291fd7146fe4175
Atharva Arya <55894364+atharva-2001@users.noreply.github.com>
2024-09-12 11:14:08 +0530

    Deployment Fixes (#90)
    * dev docker compose file only frontend testing fixed
    
    * testing new config file
    
    * new production docker compose file
    
    * Modified frontend docker configuration
    
    * Get commands from env file
    
    * Add env files
    
    * Delete extra files
    
    * Delete .flaskenv
    
    * no need for npm run start
    
    * Raise propoer error messages
    
    * edit dev env file
    
    * nginx configuration from env file
    
    * default values
    
    * syntax issues
    
    * no nginx conf in env file
    
    * nginx conf edit
    
    * nginx template
    
    * redis conf
    
    * redis commander
    
    * nginx configuration
    
    * flower css
    
    * more flower tests
    
    * more flower tests
    
    * more flower tests
    
    * static flower nginx
    
    * healths checks and more
    
    * remove secrets block
    
    * no celery user
    
    * no flower user
    
    * Remove extra items
    
    * flower and backend fixes
    
    * frontend fixes
    
    * add redis commander config
    
    * env file testing
    
    * new env file
    
    * new config
    
    * .env path issue
    
    * model file not accessible
    
    * Remove auth for dev
    
    * celery config files are not needed anymore
    
    * remove nginx setup.conf since now we depend on template
    
    * Improved docker compose dev file
    
    * Remove healthchecks for celery and backend for the time being
    
    * Rename docker compose dev
    
    * No need of dump.rdb

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,26 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// in prod uncomment this
-const { REACT_APP_BASE_URL, REACT_APP_API_URL } = process.env;
-
-// in dev uncomment this
-// const REACT_APP_BASE_URL = "http://127.0.0.1:8080";
-// const REACT_APP_API_URL = "http://127.0.0.1:8000";
-
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true,
-    origin: REACT_APP_BASE_URL || "http://0.0.0.0:8080",
-    proxy: {
-      "/api": {
-        target: REACT_APP_API_URL || "http://0.0.0.0:8080",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    base: "/",
+    server: {
+      port: 8080,
+      strictPort: true,
+      host: true,
+      origin: env.REACT_APP_BASE_URL || "http://127.0.0.1:8080",
+      proxy: {
+        "/api": {
+          target: env.REACT_APP_API_URL || "http://127.0.0.1:8000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+      watch: {
+        usePolling: true,
       },
     },
-  },
+  };
 });
--- pacmanweb/config.py
+++ pacmanweb/config.py
@@ -1,44 +1,28 @@
-import json
 import os
 import pathlib
 import yaml
+from dotenv import load_dotenv
+import secrets
 
-# or prod
-MODE = "prod"
-
+load_dotenv(f'.env')
 
 class Config:
     ROOTDIR = pathlib.Path(__file__).resolve().parent
-    ENV_NAME = ""
+    ENV_NAME = os.getenv('ENV_NAME')
 
-    secrets_fpath = ROOTDIR.parent / "secrets.json"
-    with open(secrets_fpath, "r") as secrets:
-        CREDS = json.load(secrets)
-
-    # will surpass the environment variable
-    if CREDS.get("ENV_NAME", None):
-        ENV_NAME = CREDS["ENV_NAME"]
-
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    TEST_ADS_API_KEY = CREDS["ADS_DEV_KEY"]
-    ENV_NAME = CREDS["ENV_NAME"]
-
-    if MODE == "prod":
-        CELERY_RESULT_BACKEND = "redis://redis:6379/0"
-        CELERY_BROKER_URL = f"amqp://guest:guest@rabbitmq:5672"
-        ENV_NAME = "base"  # env needs to be base for docker
-        SUBPROCESS_COMMANDS = f"micromamba run -n {ENV_NAME}  python run_pacman.py"
-
-    if MODE == "dev":
-        CELERY_RESULT_BACKEND = "redis://"
-        CELERY_BROKER_URL = f"pyamqp://"
-        SUBPROCESS_COMMANDS = f"conda run -n {ENV_NAME}  python run_pacman.py"
-
+    SECRET_KEY = os.getenv('SECRET_KEY')
     if not SECRET_KEY:
-        try:
-            SECRET_KEY = CREDS.get("SECRET_KEY", os.urandom(32))
-        except KeyError:
-            raise ValueError("No SECRET_KEY found in secrets.json or in path")
+        SECRET_KEY = secrets.token_hex()
+    TEST_ADS_API_KEY = os.getenv('ADS_DEV_KEY')
+    if not TEST_ADS_API_KEY:
+        raise ValueError("No ADS key provided. Please edit your environment file.")
+
+    if not ENV_NAME:
+        raise ValueError("No ENV_NAME provided. Please edit your environment file.")
+
+    CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+    SUBPROCESS_COMMANDS = os.getenv('SUBPROCESS_COMMANDS')
 
     # celery config
     CELERY = {}
