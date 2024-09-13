09db55871fe9fbb212ff4e1af861cf993d2863b5
KasukabeDefenceForce <abhinavohri13@gmail.com>
2024-09-13 15:51:39 +0530

    Change postion for error icon

--- frontend/src/components/util/PasswordInput.jsx
+++ frontend/src/components/util/PasswordInput.jsx
@@ -1,25 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import ErrorMessage from "../util/ErrorMessage.jsx";
 import ShowPasswordIcon from "../../assets/show.png";
 import HidePasswordIcon from "../../assets/hide.png";
 
-const PasswordInput = ({
-  label,
-  value,
-  setValue,
-  error,
-  desc,
-  showPassword,
-  toggleShowPassword,
-  disabled,
-}) => {
+const PasswordInput = ({ label, value, setValue, error, desc, disabled }) => {
   const handleOnChange = useCallback(
     (event) => {
       setValue(event.target.value);
     },
     [setValue]
   );
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className="dropdown-container">
@@ -28,6 +23,7 @@ const PasswordInput = ({
           className={`custom-input-form-label ${
             disabled ? "label-disabled" : ""
           }`}
+          style={{ color: error ? "red" : "" }}
         >
           {label}
         </label>
@@ -43,11 +39,19 @@ const PasswordInput = ({
           disabled={disabled}
           autoFocus
         />
+        {error && (
+          <img
+            src="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23dc3545'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/%3e%3c/svg%3e"
+            alt="Exclamation Icon"
+            className="error-sign"
+          />
+        )}
         <button
-          onClick={toggleShowPassword}
+          onClick={handleShowPassword}
           type="button"
           className="show-password-btn"
           disabled={disabled}
+          style={{ right: error ? "45px" : "" }}
         >
           <img
             src={showPassword ? HidePasswordIcon : ShowPasswordIcon}
