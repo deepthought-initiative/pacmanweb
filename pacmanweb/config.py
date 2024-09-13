bdf9d3cc04a21373f7ed90a26d3ba4f64c9098da
Atharva Arya <aryaatharva18@gmail.com>
2024-09-13 13:47:27 +0530

    pick up default password for mainadmin from .env

--- pacmanweb/config.py
+++ pacmanweb/config.py
@@ -7,28 +7,32 @@ import secrets
 load_dotenv(f'.env')
 
 class Config:
-    ROOTDIR = pathlib.Path(__file__).resolve().parent
+    MAINADMIN_USER_PASSWORD = os.getenv('MAINADMIN_USER_PASSWORD')
+    if not MAINADMIN_USER_PASSWORD:
+        raise ValueError("No password for mainadmin set. Please check env file.")
+
     ENV_NAME = os.getenv('ENV_NAME')
+    if not ENV_NAME:
+        raise ValueError("No ENV_NAME provided. Please edit your environment file.")
 
     SECRET_KEY = os.getenv('SECRET_KEY')
     if not SECRET_KEY:
         SECRET_KEY = secrets.token_hex()
+
     TEST_ADS_API_KEY = os.getenv('ADS_DEV_KEY')
     if not TEST_ADS_API_KEY:
         raise ValueError("No ADS key provided. Please edit your environment file.")
 
-    if not ENV_NAME:
-        raise ValueError("No ENV_NAME provided. Please edit your environment file.")
-
+    # celery config
+    CELERY = {}
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
     SUBPROCESS_COMMANDS = os.getenv('SUBPROCESS_COMMANDS')
 
-    # celery config
-    CELERY = {}
-
-    file_path = pathlib.Path(__file__).resolve()
-    PACMAN_PATH = file_path.parents[1] / "PACMan"
+    # folders
+    ROOTDIR = pathlib.Path(__file__).resolve().parent
+    CURRENT_FPATH = pathlib.Path(__file__).resolve()
+    PACMAN_PATH = CURRENT_FPATH.parents[1] / "PACMan"
     UPLOAD_FOLDER = ROOTDIR / "uploads"
     DOWNLOAD_FOLDER = ROOTDIR / "downloads"
     PANELISTS_DATA = PACMAN_PATH / "runs" / "input_panelist_data"
